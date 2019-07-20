f13c5200b993371f5de6594e9000b49f9bdc9741
zhangzhiheng <zhangzhiheng>
2019-07-20 15:52:28 +0800

    spring 初始化属性

--- src/main/java/apple/pangzi/spring/Application.java
+++ src/main/java/apple/pangzi/spring/Application.java
@@ -1,8 +1,13 @@
 package apple.pangzi.spring;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class Application {
 
     public static void main(String[] args) {
-
+        ApplicationContext context = new ClassPathXmlApplicationContext("springApplication.xml");
+        User user = (User) context.getBean("user");
+        System.out.println(user.getAddress());
     }
 }
